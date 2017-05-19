28c1fc738853487b8de530c88c30dd518ec7b557
maha-m-hamza <maha.hamza@doit.com.sa>
2017-05-19 20:04:39 +0200

    TieRope

---
+++ src/TieRope.java
@@ -0,0 +1,23 @@
+
+public class TieRope {
+	public static void main(String[] args) {
+		int[] A = new int[] { 1, 2, 3, 4, 1, 1, 3 };
+		int K = 4;
+
+		System.out.println(solution(A, K));
+	}
+
+	public static int solution(int[] A, int K) {
+		int numOfRopes = 0;
+		int ropeLengthCounter = 0;
+		for (Integer a : A) {
+			ropeLengthCounter += a;
+			if (ropeLengthCounter >= K) {
+				numOfRopes++;
+				ropeLengthCounter = 0;
+			}
+		}
+		System.out.println();
+		return numOfRopes;
+	}
+}
