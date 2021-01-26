d1d1d5d3ee616260adfc104d6f20f9bc9ba4f66d
Maha Hossam-eldien <sci.maha@gmail.com>
2021-01-26 21:17:39 +0100

    Update MaxProductOfThree.java

--- src/Codility/MaxProductOfThree.java
+++ src/Codility/MaxProductOfThree.java
@@ -2,6 +2,28 @@ package Codility;
 
 import java.util.Arrays;
 
+/**
+A non-empty zero-indexed array A consisting of N integers is given.
+
+The problem is to find the maximum product of 3 elements from given array.
+
+The length of the array is between 3 and 100,000
+
+each element of array A is an integer within the range [−1,000..1,000]
+
+    expected worst-case time complexity is O(N*log(N));
+
+    expected worst-case space complexity is O(1), 
+beyond input storage (not counting the storage required for input arguments). Example:
+
+  a[0] = -3;
+  a[1] = 7;
+  a[2] = 2;
+  a[3] = 1;
+  a[4] = 5;
+  a[5] = 7;
+the max product is a[1]*a[4]*a[5] = 245
+*/
 public class MaxProductOfThree {
 	public static void main(String[] args) {
 		int[] A = new int[] { -9, -6, 3, 2, 5, 7 };
